39f2984058137492acef83addc38c3c211d2867a
ahiezerrivas <ahiezerrivas@gmail.com>
2020-06-25 13:18:56 -0400

    Update app.js

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const fileName = "txt.txt"
 
-const errHandler = (err) => console.log(err);
+const errHandler = err => console.log(err);
 
-const dataHandler = err => console.log(data.toString());
+const dataHandler = data => console.log(data.toString());
 
 fs.readFile(fileName, (err, data) => {
     if(err)  errHandler(err)
