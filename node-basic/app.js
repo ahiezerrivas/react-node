23f29cac1f1fd49030cbc16a72615ef5070dbe17
ahiezerrivas <ahiezerrivas@gmail.com>
2020-06-25 13:02:07 -0400

    txt Video 16 app.js

--- node-basic/app.js
+++ node-basic/app.js
@@ -10,4 +10,11 @@
 const fs = require('fs')
 const fileName = "txt.txt"
 
-fs.watch(fileName, () => console.log('File Changed'))
+fs.readFile(fileName, (err, data) => {
+    if(err) {
+      console.log(err)
+    }
+  console.log(data)
+})
+
+console.log('Node js async programing');
