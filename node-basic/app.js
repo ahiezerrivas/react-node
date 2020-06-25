7c07bd9d3f2ac38918f9f83f14661a0930342dd5
ahiezerrivas <ahiezerrivas@gmail.com>
2020-06-25 13:03:01 -0400

    Update app.js

--- node-basic/app.js
+++ node-basic/app.js
@@ -14,7 +14,7 @@ fs.readFile(fileName, (err, data) => {
     if(err) {
       console.log(err)
     }
-  console.log(data)
+  console.log(data.toString());
 })
 
 console.log('Node js async programing');
