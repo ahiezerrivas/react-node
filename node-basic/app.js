ab6c7cbcd5e4d45eb5b9ddb4bd33795eb8f80975
ahiezerrivas <ahiezerrivas@gmail.com>
2020-06-25 13:11:42 -0400

    Update app.js

--- node-basic/app.js
+++ node-basic/app.js
@@ -10,11 +10,14 @@
 const fs = require('fs')
 const fileName = "txt.txt"
 
-fs.readFile(fileName, (err, data) => {
+const data = fs.readFileSync(fileName)
+console.log(data.toString())
+
+/*fs.readFile(fileName, (err, data) => {
     if(err) {
       console.log(err)
     }
   console.log(data.toString());
 })
-
+*/
 console.log('Node js async programing');
