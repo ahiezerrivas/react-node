c50ec39de9177b04a21c3f1cd811f0c8c3063196
ahiezer <ahiezerrivas@gmail.com>
2020-07-06 19:28:20 -0400

    txt video 66

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,9 +1,19 @@
 //function greeting(message) {
  // return alert(`${message} everyone`);
 //}
-//let greetting = (message) =>  alert(`${message} everyone`);
-//geeting('Good morning');
+let greetting = () =>  alert(`${message} everyone`);
+greeting('Good morning');
 
-let createBlog = (title, body) => {
-  if(!title) {
-    throw new Error('A title is required
+// let createBlog = (title, body) => {
+//   if(!title) {
+//     throw new Error('A title is required');
+//   }
+
+//   if(!body) {
+//     throw new Error('Body cant be empty');
+//   }
+
+//   return alert`${title} - ${body}`;
+// }
+
+// createBlog('Blog title', ' Blog Body');
