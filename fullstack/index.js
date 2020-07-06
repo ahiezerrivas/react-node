1e725a3b11fcb9d6f9dfb5d12765db2e4576b96b
ahiezerrivas <ahiezerrivas@gmail.com>
2020-07-06 11:42:48 -0400

    Update index.js

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,8 +1,8 @@
-
-if (true) {
-  let name = "Ryan";
-  name = 'Zen';
-  alert(name);
-}
+let fname = 'Ryan';
+let lname = 'D';
+let age = prompt("Guess Ryan's age..");
 
 
+//old way
+let result = fname + ' ' + lname + ' ' + age + 'years old';
+alert(result);
