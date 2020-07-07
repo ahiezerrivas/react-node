861fe1d7983f5f012a002d6640932a36e0c5c612
ahiezer <ahiezerrivas@gmail.com>
2020-07-06 20:18:32 -0400

    restructuring an array del video 70

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,3 +1,9 @@
-let [, ,firstmountain] = ['Everest', 'Fish Tail', 'Annapurna'];
 
-console.log(firstmountain);
+var adventureClimbing = {
+   name : 'Everest',
+   height : 8848,
+   output () {
+  console.log(`Mt. ${this.name} is ${this.height} meter tall`)
+  }
+}
+console.log(adventureClimbing);
