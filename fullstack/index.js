65640f0ef38cdcc936ee3ae732dbde7891b005e4
ahiezer <ahiezerrivas@gmail.com>
2020-07-08 19:24:06 -0400

    Mitad del txt del video 71

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,9 +1,7 @@
 
-var adventureClimbing = {
-   name : 'Everest',
-   height : 8848,
-   output () {
-  console.log(`Mt. ${this.name} is ${this.height} meter tall`)
-  }
-}
-console.log(adventureClimbing);
+var mountains =['Everest', 'Fish Tail','Annapurna'];
+var mountainsFromJapan = ['Fuji'];
+
+var allMountains = [...mountains, ...mountainsFromJapan];
+
+console.log(allMountains);
