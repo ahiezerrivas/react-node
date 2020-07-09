0a9ccdffc29bb58ab5455eba4ef1fde4bb3cd9a4
ahiezer <ahiezerrivas@gmail.com>
2020-07-08 21:18:13 -0400

    parte 1 del txt del video 72

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,6 +1,19 @@
-// rest 
-var rivers = ['Sunkoshi', 'Tamakoshi', 'Sapatakoshi'];
+//classes 
 
-var [first, ...rest] = rivers;
+//we could create a function and add methos on the function object using the prototype
 
-console.log(first);
+//in javascript functions are objects -inherit the behavior of objecct
+//whenever you create an object, the prototype object is also created behind the scene
+
+
+function Holiday(destination, days) {
+    this.destination = destination
+    this.days = days
+}
+
+Holiday.prototype.info = function() {
+    console.log(this.destination + " | " +this.days + " days");
+}
+
+var nepal = new Holiday('Nepal', 30);
+console.log(nepal.info());
