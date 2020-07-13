1f1e70c4ef8114c7ce360db430c21bc78dbde122
ahiezerrivas <ahiezerrivas@gmail.com>
2020-07-13 14:33:37 -0400

    Update Signin.js

--- react-front/src/user/Signin.js
+++ react-front/src/user/Signin.js
@@ -20,12 +20,7 @@ class Signin extends Component {
 		this.setState({ [name]: event.target.value})
 	}
     
-    authenticate  (jwt, next) {
-        if(typeof window !== "undefined") {
-            localStorage.setItem("jwt", JSON.stringify(jwt)) 
-            next()
-        }
-    }
+   
 
 	clickSubmit = event => {
         event.preventDefault();
@@ -55,20 +50,7 @@ class Signin extends Component {
 	}
 
 
-	signin = user => {
-		return fetch("http://locahols:8080/signin", {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type":"application/json"
-			},
-			body:JSON.stringify(user)
-		})
-			.then(response => {
-				return response.json()
-			})
-			.catch(err => console.log(err))
-	}
+	
 
 	signinForm = ( email, password) => (
 		<form>
