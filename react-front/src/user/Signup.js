74753aff431a6776d235395dbdc7aebe97e9a85d
ahiezerrivas <ahiezerrivas@gmail.com>
2020-07-13 14:32:05 -0400

    Update Signup.js

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from  "react";
+import {signup} from "../auth";
 
 class Signup extends Component {
 	constructor() {
@@ -40,20 +41,7 @@ class Signup extends Component {
 	}
 
 
-	signup = user => {
-		return fetch("http://locahols:8080/signup", {
-			method: "POST",
-			headers: {
-				Accept: "application/json",
-				"Content-Type":"application/json"
-			},
-			body:JSON.stringify(user)
-		})
-			.then(response => {
-				return response.json()
-			})
-			.catch(err => console.log(err))
-	}
+	
 
 	signupForm = (name, email, password) => (
 		<form>
