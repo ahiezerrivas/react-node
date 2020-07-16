6593f7b0e5a8a2b85021066cd5269bb45300082a
ahiezerrivas <ahiezerrivas@gmail.com>
2020-07-16 09:42:40 -0400

    txt 113 auth/index

--- react-front/src/auth/index.js
+++ react-front/src/auth/index.js
@@ -36,3 +36,32 @@ export const    authenticate = (jwt, next) => {
             next()
         }
     }
+
+
+export const signout = next => {
+    if(typeof window !== "undefined") localStorage.removeItem("jwt")
+    next()
+    return fetch("http://locahost:8080/signout", {
+        method: "GET"
+        
+    })
+    .then(response) => {
+        console.log('signout', response)
+        return response.json()
+    }
+    .catch(err => console.log(err))
+
+}
+
+
+export const isAuthenticated = () => {
+    if(typeof window == "undefined") {
+        return false
+    }
+    
+    if(localStorage.getItem("jwt")) {
+        return JSON.parse(localStorage.getItem("jwt"))
+     } else {
+         return false;
+     } 
+};
