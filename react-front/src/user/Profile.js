7cfcdfdb32559705675a438d71eb9cce3c26f3d6
ahiezer <ahiezerrivas@gmail.com>
2020-07-23 15:10:53 -0400

    txt 13 profile menu deleteuser apiUser

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -85,7 +85,7 @@ class Profile extends Component {
               >
                 Edit Profile
               </Link>
-              <DeleteUser />
+              <DeleteUser userId={user._id} />
             </div>
           )}
           </div>
