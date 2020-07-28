ad673a91fd1b212648ec9926e734205464d3036f
ahiezer <ahiezerrivas@gmail.com>
2020-07-28 15:27:01 -0400

    txt 135 EditProfile apiUser

--- react-front/src/user/EditProfile.js
+++ react-front/src/user/EditProfile.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { isAuthenticated } from '../auth';
-import { read } from "./apiUser"
+import { read, update } from "./apiUser"
+import {Redirect} from 'react-router-dom';
+
 class EditProfile extends Component {
 
     constructor() {
@@ -9,7 +11,8 @@ class EditProfile extends Component {
             id: "",
             name:"",
             email: "",
-            password:""
+			password:"",
+			redirectToProfile:false
         }
     }
 
@@ -17,10 +20,14 @@ class EditProfile extends Component {
         const token = isAuthenticated().token
           read(userId, token).then(data => {
             if (data.error) {
-              this.setState({ redirecToSignin: true })
+              this.setState({ redirectToProfile: true })
             }
             else {
-              this.setState({ id: data._id, name: data.name, email: data.email })
+              this.setState({ 
+				  id: data._id,
+				  name: data.name,
+				  email: data.email,
+				  error: '' })
             }
           })  
     
@@ -43,20 +50,20 @@ class EditProfile extends Component {
 		const user = {
 			 name,
 			 email,
-			 password
+			 password: password || undefined
 		};
-		console.log(user)
-		// signup(user)
-		// .then(data => {
-		// 	if(data.error) this.setState({error: data.error})
-		// 		else this.setState({
-		// 			error:"",
-		// 			name:"",
-		// 			email:"",
-		// 			password:"",
-		// 			open: true
-		// 		})
-		// })
+		// console.log(user)
+
+		const userId = this.props.match.params.userId;
+		const token = isAutheticated().token;
+
+		update(userId, token, user).then(data => {
+			if(data.error) this.setState({error: data.error})
+				else 
+				this.setState({
+					redirectToProfile: true
+				})
+		})
 	}
 
       signupForm = (name, email, password) => (
@@ -89,7 +96,12 @@ class EditProfile extends Component {
 	)
 
     render() {
-        const {  name, email, password } = this.state
+		const {  id, name, email, password } = this.state
+		
+		if(redirectToProfile){
+			return <Redirect to={`/user/${id}`}/>
+		}
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Edit Profile</h2>
--- react-front/src/user/apiUser.js
+++ react-front/src/user/apiUser.js
@@ -15,6 +15,24 @@ export const read = (userId, token) => {
  
    }
 
+   export const update = (userId, token) => {
+
+    return  fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+       method: "PUT",
+       headers: {
+         Accept: "application/json",
+         "Content-Type":"application/json",
+         Authorization: `Bearer ${token}`
+       },
+       body: JSON.stringify(user)
+     })
+     .then(response => {
+       return response.json()
+     })
+     .catch(err => console.log(err))
+ 
+   }
+
 
 export const remove = (userId, token) => {
 
