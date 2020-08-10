97f5656dc884a18857dcd065220b147f46516de4
ahiezerrivas <ahiezerrivas@gmail.com>
2020-08-10 10:07:19 -0400

    txt 157 profile

--- react-front/src/user/Profile.js
+++ react-front/src/user/Profile.js
@@ -5,6 +5,7 @@ import { read } from "./apiUser";
 import DefaultProfile from '../images/avatar.png';
 import DeleteUser from './DeleteUser';
 import FollowProfileButton from './FollowProfileButton';
+import ProfileTabs from '.ProfileTabs';
 
 class Profile extends Component {
   constructor(){
@@ -125,6 +126,14 @@ s
 		onButtonClick={this.clcikFollowButton}
 		/>
           )}
+
+	<hr />
+
+
+	<ProfileTabs 
+		followers={user.followers} 
+		following={user.following} 
+	/>
           </div>
         </div>
 
