9f657efc7e06f8adfb1ff91b2b4477bff872b701
ahiezerrivas <ahiezerrivas@gmail.com>
2020-08-17 08:29:53 -0400

    txt 166 apiPost

--- react-front/src/post/apiPost.js
+++ react-front/src/post/apiPost.js
@@ -11,3 +11,23 @@ export const create = (userId, token, post) => {
             return response.json();
         })
      
+// export const list = () => {
+//     return fetch(`${process.env.REACT_APP_API_URL}/posts`, {
+//         method: "GET"
+//     })
+//         .then(response => {
+//             return response.json();
+//         })
+//         .catch(err => console.log(err));
+// };
+
+    
+export const list = page => {
+    return fetch(`${process.env.REACT_APP_API_URL}/posts/?page=${page}`, {
+        method: "GET"
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
