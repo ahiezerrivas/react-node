7fd644ab6ad10d7c4b449a029403406e3b79573a
ahiezer <ahiezerrivas@gmail.com>
2020-08-29 23:08:25 -0400

    txt 190

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -9,7 +9,9 @@ const { getPosts,
        photo,
        singlePost,
        like,
-       unlike
+       unlike,
+       comment,
+       uncomment
       } = require("../controllers/post");
 const { requireSignin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -22,6 +24,11 @@ router.get("/posts", getPosts);
 //like unlike
 router.put("/post/like", requireSignin, like);
 router.put("/post/unlike", requireSignin, unlike);
+
+
+//comments
+router.put("/post/comment", requireSignin, comment);
+router.put("/post/uncomment", requireSignin, uncomment);
 
 
 router.post(
