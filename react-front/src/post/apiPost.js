e8e987a2ffe4a792352f8296037197fa9287d24a
ahiezer <ahiezerrivas@gmail.com>
2020-08-29 23:15:47 -0400

    txt 191

--- react-front/src/post/apiPost.js
+++ react-front/src/post/apiPost.js
@@ -119,3 +119,38 @@ export const remove = (postId, token) => {
  
    }
  
+   export const comment = (userId, token, postId, comment) => {
+    
+    return  fetch(`${process.env.REACT_APP_API_URL}/post/comment`, {
+       method: "PUT",
+       headers: {
+         Accept: "application/json",
+         "Content-Type":"application/json",
+         Authorization: `Bearer ${token}`
+       },
+       body: JSON.stringify({ userId, postId, comment })
+     })
+     .then(response => {
+       return response.json()
+     })
+     .catch(err => console.log(err))
+ 
+   }
+
+   export const uncomment = (userId, token, postId, comment) => {
+    
+    return  fetch(`${process.env.REACT_APP_API_URL}/post/uncomment`, {
+       method: "PUT",
+       headers: {
+         Accept: "application/json",
+         "Content-Type":"application/json",
+         Authorization: `Bearer ${token}`
+       },
+       body: JSON.stringify({userId, postId, comment})
+     })
+     .then(response => {
+       return response.json()
+     })
+     .catch(err => console.log(err))
+ 
+   }
