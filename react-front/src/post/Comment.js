8eaf9e7f34318f88922b6e67b55bf5c548d2ea17
ahiezerrivas <ahiezerrivas@gmail.com>
2020-09-04 11:26:02 -0400

    txt 193

--- react-front/src/post/Comment.js
+++ react-front/src/post/Comment.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { comment, uncomment } from './apiPost';
 import { isAuthenticated } from '../auth'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import DefaultProfile from "../images/avatar.jpg";
 
 class Comment extends Component {
     state = {
@@ -35,6 +36,7 @@ class Comment extends Component {
         
 
         return ( 
+            const { comments} = this.props
             <div>
                 <h2 className="mt-5 mb-5">Leave a comment</h2>
                 
@@ -44,10 +46,64 @@ class Comment extends Component {
                         <input
                           type="text"
                           onChange={this.handleChange}
+                          value={this.state.text}
                           className="form-control"
+                          placeholder="Leave a comment..."   
                           />
+                        <button className="btn btn-raised btn-success mt-2">
+                            Post
+                        </button>
                     </div>
                 </form>
+                
+           
+        
+          
+                
+                    <div className="col-md-12">
+                        <h3 className="text-primary">
+                            {comments.length} Comments
+                        </h3>
+                        <hr />
+                        {comments.map((comment, i) => (
+                            <div key={i}>
+                                <div>
+                                    <Link to={`/user/${comment.posteBy._id}`}>
+                                        <img
+                                            style={{
+                                                borderRadius: "50%",
+                                                border: "1px solid black"
+                                            }}
+                                            className="float-left mr-2"
+                                            height="30px"
+                                            width="30px"
+                                            onError={i =>
+                                                (i.target.src = `${DefaultProfile}`)
+                                            }
+                                            src={`${
+                                                process.env.REACT_APP_API_URL
+                                            }/user/photo/${comment.postedBy._id}`}
+                                            alt={comment.postedBy.name}
+                                        />
+                                        </Link>
+                                        <div>
+                                            <p className="lead">
+                                                {comment.text}
+                                            </p>
+                                         
+                                            <p className="font-italic mark">
+                                                Posted by{" "}
+                                                <Link to={`/user/${comment.postedBy._id}`}>
+                                                    {comment.postedBy.name}{" "}
+                                                </Link>
+                                                on {new Date(post.created).toDateString()}
+                                            </p>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        ))}
+                    </div>
             </div>
         )
     }
